5f4f1efd57f655746fe545c0ee42e3baa29922f6
rdmadeira <rdmadeira2@gmail.com>
2022-03-26 22:48:01 -0300

    sacó right -100px que aparecia mobiles ancho extra

--- js/main.js
+++ js/main.js
@@ -9,12 +9,11 @@
         if (visibility =='hidden') {
                 sectionContacto.style.visibility = 'visible';
                 sectionContacto.style.opacity = 1;
-                sectionContacto.style.right = '0px';
-
+                
             } else {
                 sectionContacto.style.visibility = 'hidden';
                 sectionContacto.style.opacity = 0;
-                sectionContacto.style.right = '-100vw';
-            }
+                
+                }
     }
  
