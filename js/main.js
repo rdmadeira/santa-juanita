989ff681f1d75b481c50044db97ac37e659f5bc2
rdmadeira <rdmadeira2@gmail.com>
2022-03-26 21:36:41 -0300

    se agrego main.js y se corrigio overflow-x

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
-function showContactoInputs() {
-        var contactoLink = document.getElementById('click-contacto');
+ window.onload = function() {
+    var contactoLink = document.getElementById("click-contacto");
         
-        var sectionContacto = document.getElementById('contactos');
+    contactoLink.addEventListener("click", showContactoInputs);
 
+    var sectionContacto = document.getElementById('contactos');
+    
+    function showContactoInputs() {
         var visibility = sectionContacto.style.visibility;
-        
-        if (visibility=='hidden') {
-            sectionContacto.style.visibility = 'visible';
-            sectionContacto.style.opacity = 1;
-            sectionContacto.style.right = '0px';
+        if (visibility =='hidden') {
+                sectionContacto.style.visibility = 'visible';
+                sectionContacto.style.opacity = 1;
+                sectionContacto.style.right = '0px';
 
-        } else {
-            sectionContacto.style.visibility = 'hidden';
-            sectionContacto.style.opacity = 0;
-            sectionContacto.style.right = '-100vw';
-        }
+            } else {
+                sectionContacto.style.visibility = 'hidden';
+                sectionContacto.style.opacity = 0;
+                sectionContacto.style.right = '-100vw';
+            }
+    }
 }
