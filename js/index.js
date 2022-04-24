dbbd8112ce76aec56b1312fdead6134739d2b361
rdmadeira <rdmadeira2@gmail.com>
2022-04-24 16:54:41 -0300

    se agregó instagram carrousel

--- js/index.js
+++ js/index.js
@@ -61,7 +61,22 @@ function setForm() {
     var email = document.formContacto.email;
     var data = [name.value, apellido.value, tel.value, email.value]
     console.log(data);
-    
 }
 
+showSlides();
+}
+let slideIndex = 0;
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 10000); // Change image every 10 seconds
 }
